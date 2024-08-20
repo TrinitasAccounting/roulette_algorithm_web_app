653af04ee5d00626f4b1f7f7acc42161a07fb943
Clay Mangum <clay@trinitasaccounting.com>
2024-08-20 14:20:41 -0400

    adding to the roulette table

--- client/src/components/RouletteDesign/RouletteTable.js
+++ client/src/components/RouletteDesign/RouletteTable.js
@@ -6,11 +6,21 @@ function RouletteTable() {
 
 
     return (
-        <>
-            <h1 className="table-number-red">3</h1>
-            <h1 className="table-number-black">2</h1>
-            <h1 className="table-number-red">1</h1>
-        </>
+        <div style={{ height: '10px', width: '400px' }}>
+
+            <>
+                <h1 className="table-number-red">3</h1>
+                <h1 className="table-number-black">2</h1>
+                <h1 className="table-number-red">1</h1>
+            </>
+            <>
+                <h1 className="table-number-red">6</h1>
+                <h1 className="table-number-black">5</h1>
+                <h1 className="table-number-red">4</h1>
+            </>
+
+
+        </div>
     )
 }
 
