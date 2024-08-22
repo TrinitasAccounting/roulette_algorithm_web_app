9732f55f853f7b8c9ea154503b99c5b6d1e5c8d1
Clay Mangum <clay@trinitasaccounting.com>
2024-08-22 12:03:08 -0400

    continuing to update roulette table styling

--- client/src/components/RouletteDesign/RouletteTable.js
+++ client/src/components/RouletteDesign/RouletteTable.js
@@ -18,6 +18,16 @@ function RouletteTable() {
                 <h1 className="table-number-black">5</h1>
                 <h1 className="table-number-red">4</h1>
             </>
+            <>
+                <h1 className="table-number-red">9</h1>
+                <h1 className="table-number-black">8</h1>
+                <h1 className="table-number-red">7</h1>
+            </>
+            <>
+                <h1 className="table-number-red">12</h1>
+                <h1 className="table-number-black">11</h1>
+                <h1 className="table-number-red">10</h1>
+            </>
 
 
         </div>
